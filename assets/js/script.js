f425cf1e0e477770c653b67438a936e35b0728b8
Scott Elam <alex.elam@gmail.com>
2022-05-09 17:11:19 -0700

    added border styling to css, editted elements of html to match js so that the time would display correctly, added function in js to get backgrounds of rows to change with the time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,57 @@
-var errorMessage
+var saveBtn = document.querySelector(".saveBtn");
+var clearBtn = document.querySelector(".custom-clear");
+var saveBtnAll = document.querySelector(".saveBtn");
+var rows = document.getElementsByClassName("")
+
+var nineHour = document.getElementById("9");
+var tenHour = document.getElementById("10");
+var elevenHour = document.getElementById("11");
+var twelveHour = document.getElementById("12");
+var oneHour = document.getElementById("13");
+var twoHour = document.getElementById("14");
+var threeHour = document.getElementById("15");
+var fourHour = document.getElementById("16");
+var fiveHour = document.getElementById("17");
+
+$(document).ready(function() {
+    setColor()
+});
+
+function clearData() {
+    localStorage.clear();
+
+    nineHour.textContent = "";
+    tenHour.textContent = "";
+    elevenHour.textContent = "";
+    twelveHour.textContent = "";
+    oneHour.textContent = "";
+    twoHour.textContent = "";
+    threeHour.textContent = "";
+    fourHour.textContent = "";
+    fiveHour.textContent = "";  
+};
+
+clearBtn.addEventListener("click", function() {
+    clearData();
+});
+
+function setColor() {
+    var currentHour = moment().hours()
+    $(".time-block").each(function (){
+        var blockHour = parseInt($(this).children("textarea").attr("id"))
+        if(blockHour < currentHour) {
+            $(this).children("textarea").addClass("past")
+            console.log("past");
+        };
+        if(blockHour > currentHour) {
+            $(this).children("textarea").addClass("future")
+            console.log("future");
+        };
+        if(blockHour === currentHour) {
+            $(this).children("textarea").addClass("present")
+            console.log("present");
+        };
+    });
+};
+
+$('#currentDay').text(moment().format("dddd, MMMM D, h:mm:ss a"));

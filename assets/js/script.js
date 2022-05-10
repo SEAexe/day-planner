6e4c332e37f9dd8916a44eed03e73cd52f06d926
Scott Elam <alex.elam@gmail.com>
2022-05-09 19:15:31 -0700

    added saving function to js; needs debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ var seventeenText = document.getElementById("17");
 
 $(document).ready(function() {
     setColor();
+    saveData();
 });
 
 function setColor() {
@@ -33,5 +34,54 @@ function setColor() {
         };
     });
 };
+
+function saveData() {
+
+    var nine = localStorage.getItem("nineTime");
+    var ten = localStorage.getItem("tenTime");
+    var eleven = localStorage.getItem("elevenTime");
+    var twelve = localStorage.getItem("twelveTime");
+    var thirteen = localStorage.getItem("thirteenTime");
+    var fourteen = localStorage.getItem("fourteenTime");
+    var fifteen = localStorage.getItem("fifteenTime");
+    var sixteen = localStorage.getItem("sixteenTime");
+    var seventeen = localStorage.getItem("seventeenTime");
+    
+    nineText.textContent = nine;
+    tenText.textContent = ten;
+    elevenText.textContent = eleven;
+    twelveText.textContent = twelve;
+    thirteenText.textContent = thirteen;
+    fourteenText.textContent = fourteen;
+    fifteenText.textContent = fifteen;
+    sixteenText.textContent = sixteen;
+    seventeenText.textContent = seventeen;
+    };
+    
+    for (i = 0; i < saveBtn.length; i++) {
+        saveBtn[i].addEventListener("click", function(){
+    
+        var nine = nineText.value;
+        var ten = tenText.value;
+        var eleven = elevenText.value;
+        var twelve = twelveText.value;
+        var thirteen = thirteenText.value;
+        var fourteen = fourteenText.value;
+        var fifteen = fifteenText.value;
+        var sixteen = sixteenText.value;
+        var seventeen = seventeenText.value;
+    
+    localStorage.setItem("nineTime", nine);
+    localStorage.setItem("tenTime", ten);
+    localStorage.setItem("elevenTime", eleven);
+    localStorage.setItem("twelveTime", twelve);
+    localStorage.setItem("thirteenTime", thirteen);
+    localStorage.setItem("fourteenTime", fourteen);
+    localStorage.setItem("fifteenTime", fifteen);
+    localStorage.setItem("sixteenTime", sixteen);
+    localStorage.setItem("seventeenTime", seventeen);
+    });
+};
+
 
 $('#currentDay').text(moment().format("dddd, MMMM D, h:mm:ss a"));
